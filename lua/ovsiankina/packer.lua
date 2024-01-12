c3e0f83ec0490ebf22aecb353ac30bcea63a1fe0
ovsiankina <ovsiankina@proton.me>
2024-01-12 17:38:26 +0100

    instaled-bubble-preset

--- lua/ovsiankina/packer.lua
+++ lua/ovsiankina/packer.lua
@@ -44,4 +44,9 @@ return require('packer').startup(function(use)
 		}
 	}
 	
+    -- Lualine, the pretty statusline
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 end)
